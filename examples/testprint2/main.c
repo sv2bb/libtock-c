e7b3f5520da2646a4e722ce03934afb152824a7a
Samyukta Venkat <sv2bb@virginia.edu>
2020-02-09 17:51:17 -0500

    add test programs for smart-sched

---
+++ examples/testprint2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    
+  for(int i = 0; i < 10; i++) {
+    printf("Test app 2.\n");
+  }
+
+  exit(0);
+  // Can just return here. The application will continue to execute.
+  // return 0;
+}
