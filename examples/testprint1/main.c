d4f6be3cd588aacb9c55da57c461f0ae8ebbb869
Samyukta Venkat <sv2bb@virginia.edu>
2020-06-15 11:52:40 -0400

    multiple process data -> one process

--- examples/testprint1/main.c
+++ examples/testprint1/main.c
@@ -2,15 +2,24 @@
 
 int main(void) {
     
+//  void * output_buffer = memop(13, 0);
+//  void * input_buffer = memop(12, 2);
+//
+//  int num = 0;
+//  int * num_pointer = &num;
+//  memcpy(num_pointer, input_buffer, sizeof(*num_pointer));
+//  printf("Test app 1 multiplies %d by 4. \n", num);
+//
+//  num = num * 4;
+//  memcpy(output_buffer, num_pointer, sizeof(*num_pointer));
+//
+//  exit(0);
+
   void * output_buffer = memop(13, 0);
-  void * input_buffer = memop(12, 2);
+  printf("Test app 1 starting value: 30 \n");
 
-  int num = 0;
+  int num = 30;
   int * num_pointer = &num;
-  memcpy(num_pointer, input_buffer, sizeof(*num_pointer));
-  printf("Test app 1 multiplies %d by 4. \n", num);
-
-  num = num * 4;
   memcpy(output_buffer, num_pointer, sizeof(*num_pointer));
 
   exit(0);
--- examples/testprint2/main.c
+++ examples/testprint2/main.c
@@ -2,13 +2,18 @@
 
 int main(void) {
   
-  void * input_buffer = memop(12, 0);
+  void * input_buffer_1 = memop(12, 2);
+  void * input_buffer_2 = memop(12, 1);
 
-  int num = 0;
-  int * num_pointer = &num;
-  memcpy(num_pointer, input_buffer, sizeof(*num_pointer));
+  int num1 = 0;
+  int * num1_pointer = &num1;
+  memcpy(num1_pointer, input_buffer_1, sizeof(*num1_pointer));
 
-  printf("Test app 2 verifies the answer is %d. \n", num);
+  int num2 = 0;
+  int * num2_pointer = &num2;
+  memcpy(num2_pointer, input_buffer_2, sizeof(*num2_pointer));
+
+  printf("Test app 2 verifies that %d + %d = %d. \n", num1, num2, num1+num2);
 
   exit(0);
 }
